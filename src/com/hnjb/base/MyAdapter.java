66f1dded8dadf46d77efbe1cda961bfa4d4a58ae
Administrator <Administrator@OEM-20140902BCA>
2014-09-25 13:36:27 +0800

    修复评论时候没有带出内容的问题

--- src/com/hnjb/base/MyAdapter.java
+++ src/com/hnjb/base/MyAdapter.java
@@ -61,7 +61,6 @@ public class MyAdapter extends BaseAdapter implements
 	private ArrayList<Object> data;
 	private NetworkAction request;
 	private int orderTypeTemp;
-	private int orderComment;
 	/**
 	 * 
 	 * @param object
@@ -500,7 +499,7 @@ public class MyAdapter extends BaseAdapter implements
 			// 要查看的订单类型：1.待付款，2.待发货，3.待收货，4.已完成
 			int orderType = Integer.valueOf(order.getOrderType());
 			orderTypeTemp=orderType;
-			orderComment=Integer.valueOf(order.getComments());
+			int orderComment=Integer.valueOf(order.getComments());
 //			leftBtn.setTag(R.id.tag_three, orderType);
 			leftBtn.setOnClickListener(this);
 			rightBtn.setOnClickListener(this);
@@ -572,7 +571,7 @@ public class MyAdapter extends BaseAdapter implements
 			final EditText comments = (EditText) convertView
 					.findViewById(R.id.addContent);
 			stars.setTag(product);
-			stars.setTag(R.id.tag_first, comments.getText().toString());
+			
 			addComment.setOnClickListener(new OnClickListener() {
 
 				@Override
@@ -583,6 +582,7 @@ public class MyAdapter extends BaseAdapter implements
 								"请填写评论！", 2000).show();
 						return;
 					}
+					stars.setTag(R.id.tag_first, comments.getText().toString());
 					MyApplication.comment=false;
 					((OrderEvaluate)object).publishComment(stars);
 				}
@@ -674,7 +674,7 @@ public class MyAdapter extends BaseAdapter implements
 				//如果是已完成状态该按钮为评价订单的功能
 			case 4:
 				//如果是未评价的状态才执行评价操作
-				if (orderComment==0)
+				if (order.getComments().equals("0"))
 				{
 					Intent intent2=new Intent();
 					intent2.setClass(((PersonOrder)object).getActivity(), OrderEvaluate.class);
